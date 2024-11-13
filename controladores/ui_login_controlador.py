a9772373f5f6fbdadaefa770018eeae12c7f2a86
FeliFaule <felipe.faule@gmail.com>
2024-11-13 11:43:07 -0300

    cambios x
    modifique carpeta

--- controladores/ui_login_controlador.py
+++ controladores/ui_login_controlador.py
@@ -3,7 +3,7 @@ from interfaces.ui_login import Ui_LoginWindow
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtCore import pyqtSlot
 
-
+# TODA LA LOGICA DE LA VENTA LOGIN
 
 class Login(QMainWindow, Ui_LoginWindow):
     def __init__(self): #constructor method. Se ejuecuta cuando la instancia de la clase es creada.
@@ -12,5 +12,5 @@ class Login(QMainWindow, Ui_LoginWindow):
 
 
 
-       # TODA LA LOGICA DE LA VENTA LOGIN 
         
+    
