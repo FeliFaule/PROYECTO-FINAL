d193d4104f02f5d688bcee297f42d9825e74d94f
FeliFaule <felipe.faule@gmail.com>
2024-11-13 10:13:08 -0300

    Acomodacion del proyecto
    cree las carpetas dividiendo y aclarando que la funcionalidad se va a trabajar en los controladores

---
+++ controladores/ui_login_controlador.py
@@ -0,0 +1,16 @@
+
+from interfaces.ui_login import Ui_LoginWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import pyqtSlot
+
+
+
+class Login(QMainWindow, Ui_LoginWindow):
+    def __init__(self): #constructor method. Se ejuecuta cuando la instancia de la clase es creada.
+        super().__init__() #llama al constructor de la clase QMainWindow, para inicializar las funcionalidades básicas de la ventana principal de la app.
+        self.setupUi(self) 
+
+
+
+       # TODA LA LOGICA DE LA VENTA LOGIN 
+        
---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import pyqtSlot
+from controladores.ui_login_controlador import Login
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)   
+    window = Login() 
+    window.show() 
+    sys.exit(app.exec_()) 
