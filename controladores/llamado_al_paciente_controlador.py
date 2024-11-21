3427f5a5116a4e73e4e8df5b69655f3e99f0d2a0
FeliFaule <felipe.faule@gmail.com>
2024-11-20 21:00:31 -0300

    Update llamado_al_paciente_controlador.py
    Se levanta la información de los turnos

--- controladores/llamado_al_paciente_controlador.py
+++ controladores/llamado_al_paciente_controlador.py
@@ -4,21 +4,20 @@ import time
 from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem, QMessageBox
 from PyQt5.QtCore import pyqtSlot
 from interfaces.llamado_al_paciente import Ui_MainWindow
-from .todos_los_pacientes_controlador import verPacientes
+from controladores.todos_los_pacientes_controlador import verPacientes
 
 
 class AtencionPacienteWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.cargar_turnos()
+        self.cargarTurnos()
 
 
-
-    def cargar_turnos(self):    
+    def cargarTurnos(self):    
         try:
-            with open("datos/pacientes.json", "r") as file:
-                pacientes = json.load(file)
+            with open("datos/turnos.json", "r") as file:
+                turnos = json.load(file)
                 #contenido = json.load(file)
                 #pacientes = contenido["pacientes"]
 
@@ -26,18 +25,18 @@ class AtencionPacienteWindow(QMainWindow, Ui_MainWindow):
             self.listado_turnos.setRowCount(0)  # Limpiar la tabla
 
             # Insertar los datos de los pacientes en la tabla
-            for paciente in pacientes["pacientes"]:
+            for turno in turnos["turno"]:
                 # Agregar una nueva fila en la tabla
                 row_position = self.listado_turnos.rowCount()
                 self.listado_turnos.insertRow(row_position)
 
                 # Insertar los datos del paciente en las celdas correspondientes
-                self.listado_turnos.setItem(row_position, 0, QTableWidgetItem(paciente["nombre"]))
-                self.listado_turnos.setItem(row_position, 1, QTableWidgetItem(paciente["apellido"]))
-                self.listado_turnos.setItem(row_position, 2, QTableWidgetItem(paciente["dni"]))
-                self.listado_turnos.setItem(row_position, 3, QTableWidgetItem(paciente["telefono"]))
-                self.listado_turnos.setItem(row_position, 4, QTableWidgetItem(paciente["obra_social"]))
-                self.listado_turnos.setItem(row_position, 5, QTableWidgetItem(paciente["fecha_hora"]))
+                self.listado_turnos.setItem(row_position, 0, QTableWidgetItem(turno["nombre"]))
+                self.listado_turnos.setItem(row_position, 1, QTableWidgetItem(turno["apellido"]))
+                self.listado_turnos.setItem(row_position, 2, QTableWidgetItem(turno["dni"]))
+                self.listado_turnos.setItem(row_position, 3, QTableWidgetItem(turno["telefono"]))
+                self.listado_turnos.setItem(row_position, 4, QTableWidgetItem(turno["obra_social"]))
+                self.listado_turnos.setItem(row_position, 5, QTableWidgetItem(turno["fecha_hora"]))
 
             self.listado_turnos.resizeColumnsToContents()
         
@@ -68,15 +67,12 @@ class AtencionPacienteWindow(QMainWindow, Ui_MainWindow):
         else:
             QMessageBox.warning(self, "Error", "Seleccione un turno para atender.")
 
-    def ver_todos_pacientes(self):
-        # Aquí puedes mostrar todos los pacientes en una nueva ventana o realizar cualquier acción adicional.
-        self.cargar_turnos()  # Esto recarga la lista de pacientes en la tabla
 
 
     def abrirInfoPacientes(self):
         ventanaTurno = verPacientes()
         ventanaTurno.exec_()
-        self.cargar_turnos()
+        self.cargarTurnos()
 
 
     
